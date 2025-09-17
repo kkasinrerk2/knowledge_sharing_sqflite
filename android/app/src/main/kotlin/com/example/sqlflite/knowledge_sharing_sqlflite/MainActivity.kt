f19fa69d3b2114fce5fa115e4fa933bd22887c0c
Kasina Kasinrerk <kasina@xponential.asia>
2025-09-17 16:15:41 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/sqlflite/knowledge_sharing_sqlflite/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sqlflite.knowledge_sharing_sqlflite
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter sqlflite Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black26),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('sqlflite sample'),
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              // TODO: test some sqlflite features
+            },
+            child: Text('Test button')
+          ),
+        ],
+      ),
+    );
+  }
+}
